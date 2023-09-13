1f20eb40f609b611cf888838f2217d43c61e4e02
Robert Watt <wattrg@hotmail.com>
2023-09-13 20:43:09 +1000

    Fix broken clean command

--- src/ibis/src/commands/clean.py
+++ src/ibis/src/commands/clean.py
@@ -6,7 +6,7 @@ from ibis_py_utils import read_defaults
 def main():
     directories = read_defaults("directories.json") 
     for dir in ("config_dir", "grid_dir", "flow_dir"):
-        if os.path.exists(dir):
+        if os.path.exists(directories[dir]):
             shutil.rmtree(directories[dir])
 
     if os.path.exists("__pycache__"):
