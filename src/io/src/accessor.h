7fc12e8289a0865847154ec51d68d57754f89fa5
wattrg <41663969+wattrg@users.noreply.github.com>
2023-10-14 17:00:41 +1000

    Modularised the scalar accessors for writing fluid properties to disk (#5)
    * Modularised the scalar accessors for writing fluid properties to disk
    
    * Remove file accidently added in 604c80be

---
+++ src/io/src/accessor.h
@@ -0,0 +1,49 @@
+#ifndef ACCESSOR_H
+#define ACCESSOR_H
+
+#include <map>
+#include <Kokkos_Core.hpp>
+#include "../../src/gas/src/flow_state.h"
+
+template <typename T>
+class ScalarAccessor{
+public:
+    virtual T access(const FlowStates<T>& fs, const int i) = 0;
+};
+
+
+template <typename T>
+class PressureAccess : public ScalarAccessor<T>{
+    T access (const FlowStates<T>& fs, const int i);
+};
+
+template <typename T>
+class TemperatureAccess : public ScalarAccessor<T>{
+    T access (const FlowStates<T>& fs, const int i);
+};
+
+template <typename T>
+class DensityAccess : public ScalarAccessor<T>{
+    T access (const FlowStates<T>& fs, const int i);
+};
+
+template <typename T>
+class InternalEnergyAccess : public ScalarAccessor<T> {
+    T access (const FlowStates<T>& fs, const int i);
+};
+
+template <typename T>
+class SpeedOfSoundAccess : public ScalarAccessor<T> {
+    T access (const FlowStates<T>& fs, const int i);
+};
+
+template <typename T>
+class MachNumberAccess : public ScalarAccessor<T> {
+    T access (const FlowStates<T>& fs, const int i);
+};
+
+
+template <typename T>
+std::map<std::string, std::shared_ptr<ScalarAccessor<T>>> get_accessors();
+
+#endif
--- src/io/src/vtk.cpp
+++ src/io/src/vtk.cpp
@@ -1,4 +1,5 @@
 #include "vtk.h"
+#include "accessor.h"
 
 int vtk_type_from_elem_type(ElemType type) {
     switch (type) {
@@ -10,83 +11,9 @@ int vtk_type_from_elem_type(ElemType type) {
 }
 
 template <typename T>
-class ScalarAccessor{
-public:
-    virtual const T access(const FlowStates<T>& fs, const int i) = 0;
-};
-
-template <typename T>
-class VectorAccessor{
-public:
-    virtual const T access_x(const FlowStates<T>& fs, const int i) = 0;
-    virtual const T access_y(const FlowStates<T>& fs, const int i) = 0;
-    virtual const T access_z(const FlowStates<T>& fs, const int i) = 0;
-};
-
-template <typename T>
-class PressureAccess : public ScalarAccessor<T>{
-    const T access (const FlowStates<T>& fs, const int i) {
-        return fs.gas.pressure(i);
-    }
-};
-
-template <typename T>
-class TemperatureAccess : public ScalarAccessor<T>{
-    const T access (const FlowStates<T>& fs, const int i) {
-        return fs.gas.temp(i);
-    }
-};
-
-template <typename T>
-class DensityAccess : public ScalarAccessor<T>{
-    const T access (const FlowStates<T>& fs, const int i) {
-        return fs.gas.rho(i);
-    }
-};
-
-template <typename T>
-class InternalEnergyAccess : public ScalarAccessor<T> {
-    const T access (const FlowStates<T>& fs, const int i) {
-        return fs.gas.energy(i);
-    }
-};
-
-template <typename T>
-class SpeedOfSoundAccess : public ScalarAccessor<T> {
-    const T access (const FlowStates<T>& fs, const int i){
-        return Kokkos::sqrt(1.4 * 287.0 * fs.gas.temp(i));
-    }
-};
-
-template <typename T>
-class MachNumberAccess : public ScalarAccessor<T> {
-    const T access (const FlowStates<T>& fs, const int i){
-        T a = Kokkos::sqrt(1.4 * 287.0 * fs.gas.temp(i));
-        T vx = fs.vel.x(i);
-        T vy = fs.vel.y(i);
-        T vz = fs.vel.z(i);
-        T v_mag = Kokkos::sqrt(vx*vx + vy*vy + vz*vz);
-        return v_mag / a;
-    }
-};
-
-template <typename T>
-class VelocityAccess : public VectorAccessor<T> {
-    const T access_x(const FlowStates<T>& fs, const int i) {
-        fs.vel.x(i);
-    }
-    const T access_y(const FlowStates<T>& fs, const int i) {
-        fs.vel.y(i);
-    }
-    const T access_z(const FlowStates<T>& fs, const int i) {
-        fs.vel.z(i);
-    }
-};
-
-template <typename T>
 void write_scalar_field(std::ofstream& f, 
                         const FlowStates<T> fs, 
-                        std::unique_ptr<ScalarAccessor<T>>& accessor, 
+                        std::shared_ptr<ScalarAccessor<T>> accessor, 
                         std::string name, 
                         std::string type, 
                         int num_values) 
@@ -143,6 +70,11 @@ void write_elem_type(std::ofstream& f, const Field<ElemType>& types){
 } 
 
 template <typename T>
+VtkOutput<T>::VtkOutput() {
+    m_scalar_accessors = get_accessors<T>();
+}
+
+template <typename T>
 int VtkOutput<T>::write(const FlowStates<T>& fs, 
                         const GridBlock<T>& grid, 
                         std::string plot_dir,
@@ -171,25 +103,24 @@ int VtkOutput<T>::write(const FlowStates<T>& fs,
     write_int_view(f, grid.cells().vertex_ids().offsets(), "offsets", "Int64", true);
     write_elem_type(f, grid.cells().shapes());
     f << "</Cells>" << std::endl;
+
+    // the cell data
     f << "<CellData>" << std::endl;
-    std::unique_ptr<ScalarAccessor<T>> pressure = std::unique_ptr<ScalarAccessor<T>>(new PressureAccess<T>()); 
-    std::unique_ptr<ScalarAccessor<T>> temp = std::unique_ptr<ScalarAccessor<T>>(new TemperatureAccess<T>()); 
-    std::unique_ptr<ScalarAccessor<T>> density = std::unique_ptr<ScalarAccessor<T>>(new DensityAccess<T>()); 
-    std::unique_ptr<ScalarAccessor<T>> energy = std::unique_ptr<ScalarAccessor<T>>(new InternalEnergyAccess<T>()); 
-    std::unique_ptr<ScalarAccessor<T>> a = std::unique_ptr<ScalarAccessor<T>>(new SpeedOfSoundAccess<T>()); 
-    std::unique_ptr<ScalarAccessor<T>> mach = std::unique_ptr<ScalarAccessor<T>>(new MachNumberAccess<T>()); 
-    write_scalar_field(f, fs, pressure, "pressure", "Float64", grid.num_cells());
-    write_scalar_field(f, fs, temp, "temperature", "Float64", grid.num_cells());
-    write_scalar_field(f, fs, density, "density", "Float64", grid.num_cells());
-    write_scalar_field(f, fs, energy, "energy", "Float64", grid.num_cells());
-    write_scalar_field(f, fs, a, "a", "Float64", grid.num_cells());
-    write_scalar_field(f, fs, mach, "Mach", "Float64", grid.num_cells());
+    for (auto &key_value : m_scalar_accessors){
+        std::string name = key_value.first;
+        std::shared_ptr<ScalarAccessor<T>> accessor = key_value.second;
+        write_scalar_field(f, fs, accessor, name, "Float64", grid.num_cells());
+    }
     write_vector_field(f, fs.vel, "velocity", "Float64", grid.num_cells());
     f << "</CellData>" << std::endl;
+
+    // close all the data fields
     f << "</Piece>" << std::endl;
     f << "</UnstructuredGrid>" << std::endl;
     f << "</VTKFile>";
     f.close();
+
+    // register that we've written this file
     times_.push_back(time);
     dirs_.push_back(time_dir + "/block_0.vtu");
     return 0;
@@ -199,10 +130,15 @@ template <typename T>
 void VtkOutput<T>::write_coordinating_file(std::string plot_dir) {
     std::ofstream plot_file(plot_dir + "/plot.pvd");
     plot_file << "<?xml version='1.0'?>" << std::endl;
-    plot_file << "<VTKFile type='Collection' version='0.1' byte_order='LittleEndian'>" << std::endl;
+    plot_file << "<VTKFile type='Collection' version='0.1' byte_order='LittleEndian'>" 
+              << std::endl;
     plot_file << "<Collection>" << std::endl;
     for (unsigned int i = 0; i < times_.size(); i++){
-        plot_file << "<DataSet timestep='" << times_[i] << "' group='' part='0' file='" << dirs_[i] << "'/>" << std::endl;
+        plot_file << "<DataSet timestep='" 
+                  << times_[i] 
+                  << "' group='' part='0' file='" 
+                  << dirs_[i] << "'/>" 
+                  << std::endl;
     }
     plot_file << "</Collection>" << std::endl;
     plot_file << "</VTKFile>" << std::endl;
