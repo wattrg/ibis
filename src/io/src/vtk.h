604c80beffac948f14f05b00cddd7ac50788f702
Robert Watt <wattrg@hotmail.com>
2023-10-14 15:58:24 +1000

    Modularised the scalar accessors for writing fluid properties to disk

--- src/io/src/vtk.h
+++ src/io/src/vtk.h
@@ -1,11 +1,14 @@
 #ifndef VTK_FLOW_FORMAT_H
 #define VTK_FLOW_FORMAT_H
 
+#include "accessor.h"
 #include "io.h"
 
 template <typename T>
 class VtkOutput : public FVOutput<T> {
 public:
+    VtkOutput();
+
     int write(const FlowStates<T>& fs, 
               const GridBlock<T>& grid, 
               std::string plot_dir, 
@@ -17,6 +20,7 @@ public:
 private:
     std::vector<double> times_;
     std::vector<std::string> dirs_;
+    std::map<std::string, std::shared_ptr<ScalarAccessor<T>>> m_scalar_accessors;
 };
 
 #endif 
