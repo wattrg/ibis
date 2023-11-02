1767ec5460b20744df62621757a63fdd36135401
Robert Watt <wattrg@hotmail.com>
2023-11-02 20:49:36 +1000

    Fix gmsh warnings when building grids in examples

--- examples/expansion_corner/grid.py
+++ examples/expansion_corner/grid.py
@@ -33,9 +33,9 @@ gmsh.model.geo.addCurveLoop([1,2,3,4,5])
 gmsh.model.geo.addPlaneSurface([1])
 
 # boundaries
-gmsh.model.addPhysicalGroup(1, [1,2], name="wall")
-gmsh.model.addPhysicalGroup(1, [3, 4], name="outflow")
-gmsh.model.addPhysicalGroup(1, [5], name="inflow") 
+gmsh.model.geo.addPhysicalGroup(1, [1,2], name="wall")
+gmsh.model.geo.addPhysicalGroup(1, [3, 4], name="outflow")
+gmsh.model.geo.addPhysicalGroup(1, [5], name="inflow") 
 
 # mesh size
 gmsh.model.geo.mesh.setSize([(0, 1), (0, 2), (0, 3), (0, 4), (0, 5)], 0.009)
