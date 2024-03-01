b67014a62c342a8bad83b30652d351c0f4ae840e
Robert Watt <wattrg@hotmail.com>
2024-03-01 20:45:22 +1000

    Fix post-processing gradient calculation bug, where not enough flow states for the ghost cells to fill the boundary conditions were being allocated. This closes #46

--- src/ibis/ibis/commands/post_commands/plot.cpp
+++ src/ibis/ibis/commands/post_commands/plot.cpp
@@ -54,7 +54,7 @@ void plot_vtk(json directories, std::vector<std::string> extra_vars) {
 
     GridBlock<T> grid(grid_dir + "/block_0000.su2", config.at("grid"));
     FiniteVolume<T> fv(grid, config);
-    FlowStates<T> fs(grid.num_cells());
+    FlowStates<T> fs(grid.num_total_cells());
     for (unsigned int time_idx = 0; time_idx < dirs.size(); time_idx++) {
         json meta_data;
         io.read(fs, grid, gas_model, meta_data, time_idx);
