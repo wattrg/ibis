0501e1a5a3276da742139e65a451b1d6341a7ebc
Robert Watt <wattrg@hotmail.com>
2024-03-01 20:12:59 +1000

    Update the minimal test

--- tests/minimal/uniform_stream/job.py
+++ tests/minimal/uniform_stream/job.py
@@ -1,4 +1,4 @@
-gas_model = get_gas_model("ideal_air")
+gas_model = IdealGas(species = "air")
 gas_state = GasState()
 gas_state.T = 300
 gas_state.rho = 1.225
@@ -6,22 +6,24 @@ gas_model.update_thermo_from_rhoT(gas_state)
 flow_state = FlowState(gas=gas_state, vx=1000.0, vy=500.0)
 
 config.convective_flux = ConvectiveFlux(
-    flux_calculator = FluxCalculator.Hanel,
-    reconstruction_order = 2
+    flux_calculator=Hanel(),
+    reconstruction_order=2
 )
 
+config.gas_model = gas_model
+
 config.solver = RungeKutta(
-    cfl = 0.5,
-    max_step = 100000,
-    max_time = 1.0,
-    plot_frequency = 0.1,
-    print_frequency = 200
+    cfl=0.5,
+    max_step=10,
+    max_time=1.0,
+    plot_frequency=0.1,
+    print_frequency=200
 )
 
 config.grid = Block(
-    file_name="grid.su2", 
+    file_name="grid.su2",
     initial_condition=flow_state,
-    boundaries = {
+    boundaries={
         "left": supersonic_inflow(flow_state),
         "top": supersonic_outflow(),
         "bottom": supersonic_inflow(flow_state),
