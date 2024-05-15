ed2a37fce7a10aae4e2cd67c743a876c6ac4dcea
Robert Watt <wattrg@hotmail.com>
2024-05-15 17:07:50 +1000

    Add 3D flat plate with velocity in the z direction

---
+++ examples/flat_plate_3D_turned/job.py
@@ -0,0 +1,41 @@
+gas_model = IdealGas(species="air")
+gas_state = GasState()
+gas_state.p = 1.013e3
+gas_state.T = 300.0
+gas_model.update_thermo_from_pT(gas_state)
+v = 4 * gas_model.speed_of_sound(gas_state)
+inflow = FlowState(gas=gas_state, vz=v)
+initial = FlowState(gas=gas_state, vz=v)
+max_time = 3 * 1.0 / v
+
+config.convective_flux = ConvectiveFlux(
+    flux_calculator=Ausmdv(),
+    reconstruction_order=2,
+)
+
+config.viscous_flux = ViscousFlux(enabled = True)
+
+config.gas_model = gas_model
+
+config.solver = RungeKutta(
+    method="ssp-rk3",
+    cfl = 1.0,
+    max_step = 500000,
+    max_time = max_time,
+    plot_every_n_steps = -20,
+    plot_frequency = max_time / 10,
+    print_frequency = 1000,
+)
+
+config.grid = Block(
+    file_name="grid.su2",
+    initial_condition=inflow,
+    boundaries = {
+        "inflow": supersonic_inflow(inflow),
+        "outflow": supersonic_outflow(),
+        "wall": fixed_temperature_no_slip_wall(temperature = 300),
+        # "wall": slip_wall(),
+        "sides": supersonic_outflow(),
+        "top": supersonic_outflow()
+    }
+)
