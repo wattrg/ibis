0ba69a1c6b75630ee5490dba677b5942a936a787
Robert Watt <wattrg@hotmail.com>
2024-08-06 20:36:56 +1000

    Add some more minimal tests to get the steady state solver working properly

--- tests/minimal/steady_state_uniform_stream/job.py
+++ tests/minimal/steady_state_uniform_stream/job.py
@@ -1,10 +1,15 @@
 gas_model = IdealGas(species = "air")
 gas_state = GasState()
 gas_state.T = 300
-gas_state.rho = 1.225
+gas_state.rho = 1.0
 gas_model.update_thermo_from_rhoT(gas_state)
-flow_state = FlowState(gas=gas_state, vx=1000.0, vy=500.0)
-# flow_state = FlowState(gas=gas_state, vx=0.0, vy=0.0)
+flow_state = FlowState(gas=gas_state, vx=10.0, vy=0.0)
+
+inflow_gs = GasState()
+inflow_gs.T = 310
+inflow_gs.rho = 1.0
+gas_model.update_thermo_from_rhoT(inflow_gs)
+initial = FlowState(gas=inflow_gs, vx=10.0, vy=0.0)
 
 config.convective_flux = ConvectiveFlux(
     flux_calculator=Hanel(),
@@ -15,20 +20,30 @@ config.gas_model = gas_model
 
 config.solver = SteadyState(
     cfl=0.5,
-    max_steps=10,
+    max_steps=5,
     plot_frequency=1,
-    print_frequency=1,
+    print_frequency=10,
     diagnostics_frequency=1,
-    linear_solver=Gmres(max_iters=1, tol=1e-10)
+    linear_solver=Gmres(max_iters=9*4, tol=1e-14)
 )
+
+
+# config.solver = RungeKutta(
+#     cfl = 0.5,
+#     max_step=100,
+#     max_time=1.0,
+#     plot_every_n_steps=1,
+#     print_frequency=1
+# )
 
 config.grid = Block(
     file_name="grid.su2",
-    initial_condition=flow_state,
+    initial_condition=initial,
     boundaries={
         "left": supersonic_inflow(flow_state),
         "top": supersonic_outflow(),
-        "bottom": supersonic_inflow(flow_state),
+        # "bottom": supersonic_inflow(flow_state),
+        "bottom": supersonic_outflow(),
         "right": supersonic_outflow()
     }
 )
