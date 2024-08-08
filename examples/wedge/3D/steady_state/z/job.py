b17bdccad911818c09d18bca43fa32723340d09e
Robert Watt <wattrg@hotmail.com>
2024-08-08 14:20:27 +1000

    Add 3D steady-state examples

--- examples/wedge/3D/steady_state/z/job.py
+++ examples/wedge/3D/steady_state/z/job.py
@@ -1,25 +1,28 @@
 mach = 3.0
 T = 300
-gas_model = IdealGas(R=287.0)
+n_flows = 3
+n_plots = 10
+length = 1.0
+gas_model = IdealGas(R = 287.0)
 gas_state = GasState()
 gas_state.rho = 1.225
 gas_state.T = T
 gas_model.update_thermo_from_rhoT(gas_state)
-vx = mach * gas_model.speed_of_sound(gas_state)
-flow_state = FlowState(gas=gas_state, vx=vx)
+v = mach * gas_model.speed_of_sound(gas_state)
+flow_state = FlowState(gas=gas_state, vz=v)
 
 config.convective_flux = ConvectiveFlux(
-    flux_calculator=Hanel(),
-    reconstruction_order=2
+    flux_calculator=Ausmdv(),
+    reconstruction_order=1
 )
 
 config.gas_model = gas_model
 
 config.solver = SteadyState(
-    cfl=10.0,
-    max_steps=1000,
-    print_frequency=10,
-    plot_frequency=100,
+    cfl=20.0,
+    max_steps=10000,
+    print_frequency=100,
+    plot_frequency=1000,
     diagnostics_frequency=1,
     tolerance=1e-10,
     linear_solver=Gmres(tol=1e-1, max_iters=100)
@@ -28,11 +31,12 @@ config.solver = SteadyState(
 config.grid = Block(
     file_name="grid.su2",
     initial_condition=flow_state,
-    boundaries={
+    boundaries = {
         "inflow": supersonic_inflow(flow_state),
         "outflow": supersonic_outflow(),
-        "wall": slip_wall(),
+        "ramp": slip_wall(),
+        "symmetry": slip_wall(),
+        "sides": slip_wall(),
+        "top": supersonic_outflow(),
     }
 )
-
-config.io = IO(flow_format="native_text")
