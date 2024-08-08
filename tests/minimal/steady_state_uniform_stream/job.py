8c3b44ede1a0a2609b0d74b3de1d3c08546e4483
Robert Watt <wattrg@hotmail.com>
2024-08-08 11:15:29 +1000

    Update minimal tests and examples of steady state solver with bug fixes

--- tests/minimal/steady_state_uniform_stream/job.py
+++ tests/minimal/steady_state_uniform_stream/job.py
@@ -1,15 +1,15 @@
-gas_model = IdealGas(species = "air")
+gas_model = IdealGas(R=2.87103245856353623822e+02)
 gas_state = GasState()
 gas_state.T = 300
 gas_state.rho = 1.0
 gas_model.update_thermo_from_rhoT(gas_state)
-flow_state = FlowState(gas=gas_state, vx=10.0, vy=0.0)
+flow_state = FlowState(gas=gas_state, vx=1000.0, vy=0.0)
 
 inflow_gs = GasState()
 inflow_gs.T = 310
 inflow_gs.rho = 1.0
 gas_model.update_thermo_from_rhoT(inflow_gs)
-initial = FlowState(gas=inflow_gs, vx=10.0, vy=0.0)
+initial = FlowState(gas=inflow_gs, vx=1000.0, vy=0.0)
 
 config.convective_flux = ConvectiveFlux(
     flux_calculator=Hanel(),
@@ -19,18 +19,19 @@ config.convective_flux = ConvectiveFlux(
 config.gas_model = gas_model
 
 config.solver = SteadyState(
-    cfl=0.5,
-    max_steps=5,
-    plot_frequency=1,
-    print_frequency=10,
+    cfl=1.0,
+    max_steps=1000,
+    plot_frequency=10,
+    print_frequency=100,
     diagnostics_frequency=1,
-    linear_solver=Gmres(max_iters=9*4, tol=1e-14)
+    linear_solver=Gmres(max_iters=36, tol=1e-14)
 )
 
 
 # config.solver = RungeKutta(
-#     cfl = 0.5,
-#     max_step=100,
+#     method="euler",
+#     cfl = 1.0,
+#     max_step=1,
 #     max_time=1.0,
 #     plot_every_n_steps=1,
 #     print_frequency=1
@@ -41,9 +42,10 @@ config.grid = Block(
     initial_condition=initial,
     boundaries={
         "left": supersonic_inflow(flow_state),
-        "top": supersonic_outflow(),
-        # "bottom": supersonic_inflow(flow_state),
-        "bottom": supersonic_outflow(),
-        "right": supersonic_outflow()
+        "right": supersonic_outflow(),
+        "top": slip_wall(),
+        "bottom": slip_wall(),
+        # "top": supersonic_outflow(),
+        # "bottom": supersonic_outflow(),
     }
 )
