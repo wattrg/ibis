1e1663b3740893c2e806776759d0b7e5457a050f
Robert Watt <wattrg@hotmail.com>
2025-03-17 10:38:08 +1000

    Don't initialise doctest mpi for the plain parallel_unittests

--- src/parallel/test/unittest.cpp
+++ src/parallel/test/unittest.cpp
@@ -1,18 +1,9 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 
 #include <doctest/doctest.h>
-#ifdef Ibis_ENABLE_MPI
-#include <doctest/extensions/doctest_mpi.h>
-#endif
-
 #include <Kokkos_Core.hpp>
 
 int main(int argc, char* argv[]) {
-// MPI_Init(&argc, &argv);
-#ifdef Ibis_ENABLE_MPI
-    doctest::mpi_init_thread(argc, argv, MPI_THREAD_MULTIPLE);
-#endif
-
     Kokkos::initialize(argc, argv);
     doctest::Context ctx;
 
@@ -20,10 +11,6 @@ int main(int argc, char* argv[]) {
     int res = ctx.run();
 
     Kokkos::finalize();
-
-#ifdef Ibis_ENABLE_MPI
-    doctest::mpi_finalize();
-#endif
 
     return res;
 }
