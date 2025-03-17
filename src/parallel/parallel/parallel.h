fae43f253a8ac33247a61c82b61cd9e0d25b07c0
Robert Watt <wattrg@hotmail.com>
2025-03-17 14:59:52 +1000

    Fix function annotations in parallel library to avoid compilation warnings from cuda

--- src/parallel/parallel/parallel.h
+++ src/parallel/parallel/parallel.h
@@ -14,20 +14,20 @@ template <class MemModel>
 void finalise();
 
 template <class FunctorType>
-KOKKOS_INLINE_FUNCTION void parallel_for(const std::string& str, const size_t work_count,
-                                         FunctorType functor) {
+inline void parallel_for(const std::string& str, const size_t work_count,
+                         FunctorType functor) {
     Kokkos::parallel_for(str, work_count, functor);
 }
 
 template <class FunctorType, class PolicyType>
-KOKKOS_INLINE_FUNCTION void parallel_for(const std::string& str, PolicyType policy,
-                                         FunctorType functor) {
+inline void parallel_for(const std::string& str, PolicyType policy,
+                         FunctorType functor) {
     Kokkos::parallel_for(str, policy, functor);
 }
 
 template <class ReducerType, class MemModel, class FunctorType>
-KOKKOS_INLINE_FUNCTION auto parallel_reduce(const std::string& str,
-                                            const size_t work_count, FunctorType functor)
+inline auto parallel_reduce(const std::string& str,
+                            const size_t work_count, FunctorType functor)
     -> typename ReducerType::scalar {
     using reducer_type = typename ReducerMap<ReducerType, MemModel>::reducer_type;
     reducer_type reducer;
@@ -35,8 +35,8 @@ KOKKOS_INLINE_FUNCTION auto parallel_reduce(const std::string& str,
 }
 
 template <class ReducerType, class MemModel, class FunctorType, class PolicyType>
-KOKKOS_INLINE_FUNCTION auto parallel_reduce(const std::string& str, PolicyType policy,
-                                            FunctorType functor) ->
+inline auto parallel_reduce(const std::string& str, PolicyType policy,
+                            FunctorType functor) ->
     typename ReducerType::scalar_type {
     using reducer_type = typename ReducerMap<ReducerType, MemModel>::reducer_type;
     reducer_type reducer;
