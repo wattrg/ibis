2d60d7d8d74fb404f76be45e369ed673c7734459
Robert Watt <wattrg@hotmail.com>
2026-02-19 07:08:01 +1000

    Add mpi example

---
+++ examples/wedge/2D/steady_state_dist/job.py
@@ -0,0 +1,51 @@
+mach = 3.0
+T = 300
+gas_model = IdealGas(R=287.0)
+gas_state = GasState()
+gas_state.rho = 1.225
+gas_state.T = T
+gas_model.update_thermo_from_rhoT(gas_state)
+vx = mach * gas_model.speed_of_sound(gas_state)
+flow_state = FlowState(gas=gas_state, vx=vx)
+
+config.convective_flux = ConvectiveFlux(
+    flux_calculator=Hanel(),
+    reconstruction_order=2
+)
+
+config.gas_model = gas_model
+
+config.solver = SteadyState(
+    cfl=10.0,
+    max_steps=1000,
+    print_frequency=10,
+    plot_frequency=100,
+    diagnostics_frequency=1,
+    tolerance=1e-10,
+    linear_solver=Gmres(tol=1e-1, max_iters=100)
+)
+
+config.grids = [
+    Block(
+        file_name="partitioned_grid/block_0.su2",
+        cell_map_file="partitioned_grid/cell_map_block_0",
+        initial_condition=flow_state,
+        boundaries={
+            "inflow": supersonic_inflow(flow_state),
+            "outflow": supersonic_outflow(),
+            "wall": slip_wall(),
+        }
+    ),
+    Block(
+        file_name="partitioned_grid/block_1.su2",
+        cell_map_file="partitioned_grid/cell_map_block_1",
+        initial_condition=flow_state,
+        boundaries={
+            "inflow": supersonic_inflow(flow_state),
+            "outflow": supersonic_outflow(),
+            "wall": slip_wall(),
+        }
+    )
+]
+
+config.io = IO(flow_format="native_text")
